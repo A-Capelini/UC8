97bb7982d05b47376e724b482ccdbabf14273bf0
A-Capelini <anderson_capelini@yahoo.com.br>
2022-02-14 20:46:37 -0300

    primeiro test Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+// variável, seu valor pode ser alterado no decorrer  do programa
+let nomeEstudante = "Helena";
+console.log(nomeEstudante);
+
+// constante, seu valor não pode ser alterado
+const nomeDoEstudante = "Helena";
+console.log(nomeDoEstudante);
+
+//Em outras linguagens, portanto, você encontrará    algo semelhante a:
+String: nomeEstudante = "Helena"; 
+*/
+
+
+/*
+// Usando o for
+let listaDeEstudantes = ["Helena", "Chico", "Mário"];
+let    quantidadeDeEstudantes =  listaDeEstudantes.length;
+//console.log(quantidadeDeEstudantes);
+for (let indice = 0; indice < quantidadeDeEstudantes;
+    indice++) {
+        const alunoCorrente = listaDeEstudantes[indice];
+        console.log(alunoCorrente);
+    }
+*/
+
+/*
+//Com o do:
+let listaDeEstudantes = ["Helena", "Chico", "Mário"];
+let    quantidadeDeEstudantes =  listaDeEstudantes.length;
+let inicio = 0;
+
+do {
+    console.log(listaDeEstudantes[inicio]);
+        inicio++;
+   } while (inicio < quantidadeDeEstudantes);
+
+*/
+
+
