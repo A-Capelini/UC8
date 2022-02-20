44b5615172342449f5fe43c9cd9eb1616361e9cf
A-Capelini <anderson_capelini@yahoo.com.br>
2022-02-20 14:39:33 -0300

    3 commit feito por main inserido funão inscricao

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+console.log("por favor escolha uma opção");
+console.log("1 - Para realizar Inscrição");
+console.log("2 - Para visualizar eventos");
+console.log("3 - Para Sair");
+
+
 var opcao = 1;
 function realizarInsc ()
 {
+    // variavel idade deveria ser inserida pelo usuario 
    let idade = 18;
     if (idade >= 18) {
         console.log("Cadastro permitido!");
+let listaDeInscrito = ["Lucas","Francisco","Anderson","Flávia","Chico", "Mário", "José", "Maria"];
+let quantidadeDeinscritos = listaDeInscrito.length;
+if (quantidadeDeinscritos < 100) {
+   // aqui deveria ter um comando para o usuaria colocar os seus dados e ser capturado pelo sistema e armazenado
+      listaDeInscrito.push("João");
+    console.log("Incrição realizada com sucesso")
+    console.log("Lista de insscrito até o momento");
+    console.log(listaDeInscrito);
+} else {
+    console.log("Cadastro não realizado, limite de inscrição excedido");
+}
+
               }
     else {
         console.log("Cadastro não permitido, menor de idade!");
       }
 }
+
+//inicio incerção
+//fim inserção
+
+
 
 if ((opcao == 1 ) || (opcao == 2) || (opcao == 3 ))
 {
